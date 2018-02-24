b25963f45696a89d2aa5e48b0c356d74a9d6dfcf
Jehu Pacheco <jehu.pacheco@pucp.pe>
2018-02-23 23:36:17 -0500

    Reaplce JSON type in migration for string MySQl5.6

--- database/migrations/2018_02_23_172342_create_products_table.php
+++ database/migrations/2018_02_23_172342_create_products_table.php
@@ -18,7 +18,7 @@ class CreateProductsTable extends Migration
             $table->string('name');
             $table->string('description');
             $table->float('price');
-            $table->json('images');
+            $table->string('images');
             $table->timestamps();
         });
     }
