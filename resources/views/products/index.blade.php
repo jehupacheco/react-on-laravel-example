d8a1b266ebdbb6c13a8ca0ddfaffe83e37139acd
Jehu Pacheco <jehu.pacheco@tektonlabs.com>
2018-02-28 15:18:03 -0500

    Safe cart parsing from backend

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,13 +1,18 @@
 @extends('base')
 
 @php
+    Debugbar::info($products);
+    Debugbar::info(Cart::content());
+
     $app = ReactRenderer::reactRenderComponentArray('products.index', [
         'props' => [
             'products' => $products,
             'cart' => Cart::content(),
             'total' => Cart::total(),
         ],
+        'trace' => true,
     ]);
+
 @endphp
 
 @section('extra-css')
